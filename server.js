17d117eb4c3501afed8802ab5414da50462d8327
derekolson09 <d.g.olson09@gmail.com>
2019-07-15 22:41:00 -0700

    update(deploy): Building prod build

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
-//Connect to Database
+// Connect to Database
 connectDB();
 
-//Init Middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactManager API' }));
-
+// Define routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve static assets in prod
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
