cb0949c5dab84d409c116dc6c002aecbf1564e87
derekolson09 <d.g.olson09@gmail.com>
2019-07-15 22:20:22 -0700

    update(app): Initialization of a lot of stuff

--- README.md
+++ README.md
@@ -10,5 +10,37 @@ Builds the client and the server on ports 3000 and 5000 respectively. This is wa
 You will need to create your own config folder and inside it have a db.js and a default.json.
 Default.json needs to include a MongoDB connection string and the and jwtSecret (encryption key).
 db.js needs to include a mongoose connection function that module exports with the name connectDB.
+Example db.js:
+```js
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        })
+
+        console.log('MongoDB Connected...')
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+
+module.exports = connectDB;
+```
+
+example default.json:
+```js
+{
+    "mongoURI": "[INSERT GENERATED MONGO CONNECTION STRING HERE]",
+    "jwtSecret": "[INSERT HASH KEY HERE]"
+}
+```
 
 Good luck and have fun.
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,6 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
+import setAuthToken from '../../utils/setAuthToken';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -23,15 +25,72 @@ const AuthState = props => {
 
     const [state, dispatch] = useReducer(authReducer, initialState);
 
-    // Load User
-
     // Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/users', formData, config);
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
 
-    // Login User
+            loadUser();
 
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg
+            })
+        }
+    }
+
+    // Load User
+    const loadUser = async () => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+        try {
+            const res = await axios.get('/api/auth');
+
+            dispatch({ type: USER_LOADED, payload: res.data });
+        } catch (err) {
+            dispatch({ type: AUTH_ERROR });
+        }
+    }
+
+    //Login user
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/auth', formData, config);
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+
+            loadUser();
+
+        } catch (err) {
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err.response.data.msg
+            })
+        }
+    }
     // Logout 
+    const logout = () => dispatch({ type: LOGOUT });
 
     // Clear Errors
+    const clearError = () => dispatch({ type: CLEAR_ERRORS });
 
     return (
         <AuthContext.Provider
@@ -41,6 +100,11 @@ const AuthState = props => {
                 loading: state.loading,
                 user: state.user,
                 error: state.error,
+                register,
+                login,
+                logout,
+                clearError,
+                loadUser
             }}>
             {props.children}
         </AuthContext.Provider>
